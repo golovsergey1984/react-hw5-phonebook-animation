2462f8e3be070984f41a3b82d6f0ab757fb0f2e8
golovsergey1984 <51264173+golovsergey1984@users.noreply.github.com>
2020-06-05 12:56:10 +0300

    Updates

---
+++ precache-manifest.7f3f3eaa70474f23a45475b0810af375.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb747692650ba0f821da7af5386c6297",
+    "url": "/react-hw5-phonebook-animation/index.html"
+  },
+  {
+    "revision": "1aa3fa3f4cfc0a54288c",
+    "url": "/react-hw5-phonebook-animation/static/css/main.ba75d7c3.chunk.css"
+  },
+  {
+    "revision": "c41da484b065ca233140",
+    "url": "/react-hw5-phonebook-animation/static/js/2.ead4b89e.chunk.js"
+  },
+  {
+    "revision": "dfd9de20f4ff60fc8200b143aaf0a20e",
+    "url": "/react-hw5-phonebook-animation/static/js/2.ead4b89e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aa3fa3f4cfc0a54288c",
+    "url": "/react-hw5-phonebook-animation/static/js/main.4c284304.chunk.js"
+  },
+  {
+    "revision": "20f25607b1981a7c0838",
+    "url": "/react-hw5-phonebook-animation/static/js/runtime-main.4cc80057.js"
+  }
+]);
